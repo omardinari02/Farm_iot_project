7f722405a401f971906f8e379a8784724018c2cb
Omar <omar.dinari.345@gmail.com>
2025-02-19 14:46:10 +0100

    Normalize line endings

---
+++ nodered/settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Chiave per crittografare le credenziali (obbligatoria)
+    credentialSecret: "secret_iot_123", 
+  
+    // Credenziali personalizzate
+    telegramBot: {
+      token: "7881215525:AAGnwgzHjsBIyTGGJZiG8bPxIFGthY_Wl78" 
+    },
+  
+  };
